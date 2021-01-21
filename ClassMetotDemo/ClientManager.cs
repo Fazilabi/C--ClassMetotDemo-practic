29ce2e317191f5a8b7bbe5dfe52175a3dd7e0312
Fazilabi <fazilag@code.edu.az>
2021-01-21 16:06:45 +0400

    ClassMetotDemo-16.01.21

---
+++ ClassMetotDemo/ClientManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class ClientManager
+    {
+        public void AddClient(Client client)
+        {
+            Console.WriteLine("Client added >>>" + 
+                                "Id :"+ client.Id + " // "+
+                                " Name : " + client.Name + " // " +
+                                " Surname : " + client.Surname + " // " +
+                                " Age : " + client.Age + " // " +
+                                " City : " + client.City  );
+
+        }
+        public void ShowClientInfos(Client client)
+        {
+            Console.WriteLine("Client added >>>" +
+                                "Id :" + client.Id + " // " +
+                                " Name : " + client.Name + " // " +
+                                " Surname : " + client.Surname + " // " +
+                                " Age : " + client.Age + " // " +
+                                " City : " + client.City);
+
+
+        }
+        public void DeleteClient(Client client)
+        {
+            Console.WriteLine("Client added >>>" +
+                                "Id :" + client.Id + " // " +
+                                " Name : " + client.Name + " // " +
+                                " Surname : " + client.Surname + " // " +
+                                " Age : " + client.Age + " // " +
+                                " City : " + client.City);
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client1 = new Client { Id = 1, Name = "Ali", Surname = "Baran", Age = 30, City = "Baku"};
+            Client client2 = new Client { Id = 2, Name = "Kerem", Surname = "Bilgin", Age = 33, City = "Izmir"};
+            Client client3 = new Client { Id = 3, Name = "Turgay", Surname = "Mustafa", Age = 29, City = "Istanbul"};
+            Client client4 = new Client { Id = 4, Name = "Hasan", Surname = "Ahmet", Age = 35, City = "Antalya"};
+            Client client5 = new Client { Id = 5, Name = "Oguz", Surname = "Turan", Age = 28, City = "Van" };
+
+            ClientManager clientManager = new ClientManager();
+            clientManager.AddClient(client1);
+            clientManager.AddClient(client2);
+            clientManager.AddClient(client3);
+            clientManager.AddClient(client4);
+            clientManager.AddClient(client5);
+            Console.WriteLine("******************************************************************************************");
+
+            clientManager.ShowClientInfos(client1);
+            clientManager.ShowClientInfos(client2);
+            clientManager.ShowClientInfos(client3);
+            clientManager.ShowClientInfos(client4);
+            clientManager.ShowClientInfos(client5);
+            Console.WriteLine("******************************************************************************************");
+
+
+            clientManager.DeleteClient(client2);
+            clientManager.DeleteClient(client4);
+        }
+    }
+}
